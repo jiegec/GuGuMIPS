ca71b368d46d6778596f9ceb9a0ec942c798268c
Jiajie Chen <noc@jiegec.ac.cn>
2019-07-09 02:07:02 +0800

    Enrich tests for bit and move instructions

--- testbench/test_bit.s
+++ testbench/test_bit.s
@@ -1,7 +1,7 @@
-	.org 0x0
-	.global _start
-	.set noat
-	.set noreorder
+    .org 0x0
+    .global _start
+    .set noat
+    .set noreorder
 _start:
     ori $1, $0, 0x8000   # ans: $1=0x00008000
     ori $2, $0, 0x0800   # ans: $2=0x00000800
@@ -14,4 +14,12 @@ _start:
     nor $9, $1, $1       # ans: $9=0xffff7fff
     andi $10, $1, 0x8888 # ans: $10=0x00008000
     xori $11, $1, 0x8888 # ans: $11=0x00000888
+    lui $1, 0xf00f       # ans: $1=0xf00f0000
+    ori $2, $1, 0xf000   # ans: $2=0xf00ff000
+    and $3, $1, $2       # ans: $3=0xf00f0000
+    xor $4, $1, $2       # ans: $4=0x0000f000
+    nor $5, $3, $4       # ans: $5=0x0ff00fff
+    or  $6, $5, $4       # ans: $6=0x0ff0ffff
+    andi $6, $6, 0xff00  # ans: $6=0x0000ff00
+    xori $6, $6, 0x0f0f  # ans: $6=0x0000f00f
     
--- testbench/test_move.s
+++ testbench/test_move.s
@@ -14,3 +14,28 @@ _start:
     mflo $3              # ans: $3=0x00000800
     mfhi $4              # ans: $4=0x00008000
     mflo $5              # ans: $5=0x00000800
+
+    lui $1, 0xf00f       # ans: $1=0xf00f0000
+    lui $2, 0x5555       # ans: $2=0x55550000
+    ori $3, $2, 0xaaaa   # ans: $3=0x5555aaaa
+
+    movz $3, $1, $3
+    ori  $3, $3, 0x0000  # ans: $3=0x5555aaaa
+
+    movz $3, $1, $0      # ans: $3=0xf00f0000
+
+    movn $3, $2, $2      # ans: $3=0x55550000
+    movn $3, $1, $0
+    ori  $3, $3, 0x0000  # ans: $3=0x55550000
+
+    lui $3, 0xffac       # ans: $3=0xffac0000
+    mthi $3 
+    mthi $2
+    mthi $1
+    mfhi $4              # ans: $4=0xf00f0000
+
+    mtlo $1
+    mtlo $2
+    mtlo $3
+    mflo $5              # ans: $5=0xffac0000
+    mfhi $5              # ans: $5=0xf00f0000
