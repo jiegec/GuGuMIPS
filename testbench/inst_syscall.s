7ffcd5cd3be313caa5b8ea78d7ab70b82c784251
Jiajie Chen <noc@jiegec.ac.cn>
2019-07-12 00:55:16 +0800

    Enrich inst syscall tset

--- testbench/inst_syscall.s
+++ testbench/inst_syscall.s
@@ -3,18 +3,25 @@
     .set noat
     .set noreorder
 _start:
-    ori $1, $0, 0x8000     # ans: $1=0x00008000
+    ori $1, $0, 0x8000      # ans: $1=0x00008000
     syscall
-    ori $2, $0, 0x1234     # ans: $2=0x00001234
+    ori $2, $0, 0x1234      # ans: $2=0x00001234
+    ori $2, $29, 0x0000     # ans: $2=0x0x00000008
+    syscall
+    ori $2, $3, 0x1234      # ans: $2=0x1234
+    ori $2, $29, 0x0000     # ans: $2=0x00000008
+    syscall
+    ori $2, $29, 0x0000     # ans: $2=0x00000008
 
     .org 0x380
     # add epc
-    mfc0 $30, $14          # ans: $30=0x00000004
-    addi $30, $30, 0x4     # ans: $30=0x00000008
+    mfc0 $30, $14          # ans: $30=pc
+    addi $30, $30, 0x4     # ans: $30=pc+4
     mtc0 $30, $14
     # read exc code
     mfc0 $29, $13          # skip
     andi $29, $29, 0x007c  # skip
-    srl $29, $29, 2        # ans: $29=0x00000008
+    srl $29, $29, 2        # ans: $29=0x00000008 syscall
     eret
+    ori $3, $0, 0x4321
     
