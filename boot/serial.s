94d7a8301cb2b2fc9ae30b5c4b800b34feb46382
Jiajie Chen <noc@jiegec.ac.cn>
2019-07-19 22:00:55 +0800

    Fix bootrom vma from 0 to 0xbfc00000

--- boot/serial.s
+++ boot/serial.s
@@ -1,11 +1,15 @@
     .org 0x0
-    .global _start
+    .global __start
     .set noat
     .set noreorder
-_start:
+__start:
     li $1, 0x61
     li $2, 0xbfd00000
 
 loop:
     sw $1, 0x0($2)
     j loop
+ 
+
+    .org 0x380
+    j __start
