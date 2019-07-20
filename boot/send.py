cfa90f2033e89992245a68832d583555bb4ba777
Jiajie Chen <noc@jiegec.ac.cn>
2019-07-21 03:27:57 +0800

    Improve send.py tool

--- boot/send.py
+++ boot/send.py
@@ -3,8 +3,25 @@
 import sys
 import os
 import struct
+import serial
+import select
+
+n = 1024
+timeout = 0.01
+
+if len(sys.argv) != 3:
+    print('Usage: send.py file tty')
+    sys.exit()
+
+out = serial.Serial(sys.argv[2], 115200, timeout=timeout)
 
 size = os.path.getsize(sys.argv[1])
-os.write(1, struct.pack('>I', size))
+out.write(struct.pack('>I', size))
 with open(sys.argv[1], 'rb') as f:
-    os.write(1, f.read())
+    data = f.read()
+    for i in range(0, len(data), n):
+        out.write(data[i:i+n])
+        print('.', end='', flush=True)
+
+out.close()
+os.execlp('screen', 'screen', sys.argv[2], '115200')
